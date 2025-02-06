ae55c9078be3fbf8ee1d4702cb3d7e8606b0d239
fan912 <scyfz2@nottingham.edu.cn>
2025-02-06 16:06:22 +0000

    Site updated: 2025-02-06 16:06:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/06/test/","2025/01/20/我的第一篇博客/","2025/02/06/hello-world/"];function toRandomPost(){
+var posts=["2025/02/06/hello-world/","2025/01/20/我的第一篇博客/","2025/02/06/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
